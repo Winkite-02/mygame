9018c2292695a05b13c69971cdba05dd652fb49a
lsy <seungyeonn11@naver.com>
2022-10-24 10:57:57 +0900

    반복문 연습

---
+++ fortest.py
@@ -0,0 +1,23 @@
+# 구구단 몇 단을 외워볼까요?
+# 입력받은 단수로 구구단 출력하기
+dan = 2
+for i in range(9):
+    dab = dan*(i+1)
+    print(f"{dan} X {(i+1)} = {dab}")
+
+# 헬로 월드를 10번 출력해보기
+print("hello world")
+print("hello world")
+print("hello world")
+'...10번 반복'
+# 헬로 월드를 앞에 숫자를 붙여서 카운팅
+'1. hello world'
+'2. hello world'
+'3. hello world'
+'...몇번'
+
+# 조건문까지 합쳐서
+# 10번 찍어서 안넘어가는 나무 없다
+# 1번 찍고 ~ 2번 찍고~ ...
+# 만약 10번을 찍었을 때
+# 나무가 넘어간다~ 출력
